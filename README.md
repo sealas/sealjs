874a55e657ed829be08ac53d3eb34a0fa8d94c1a
Daniel Khalil <d.khalil@brainsware.at>
2020-07-15 13:51:14 +0200

    feat: itemchannel and docs

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # sealjs
+
+```
+import { ItemChannel } from 'sealjs'
+
+const channel = new ItemChannel()
+
+channel.on('all_items', (items) => {
+  // Handling all items here!
+})
+
+channel.on('add_item', (item) => {
+  // Handling single new item
+})
+
+channel.on('update_item', (item) => {
+  // Handling single updated item
+})
+
+channel.on('add_item', (id) => {
+  // Handling single deleted item
+})
+```
+
+
+```
+await channel.connect(emailFromForm, passFromForm)
+
+try {
+  const result = await channel.create({
+    new: 'item'
+  })
+
+  // handle new item with id: result.id
+} catch (e) {
+  console.error('Oh no!', e)
+}
+
+try {
+  const result = await channel.update(ItemID, {
+    updated: 'item'
+  })
+
+  // handle updated item
+} catch (e) {
+  console.error('Oh no!', e)
+}
+
+try {
+  const result = await channel.delete(ItemID)
+
+  // handle deleted item
+} catch (e) {
+  console.error('Oh no!', e)
+}
+```
--- src/itemChannel.js
+++ src/itemChannel.js
@@ -4,13 +4,26 @@ import crypto from './crypto'
 
 export default class {
   constructor () {
+    this.bindings = []
+
     this.email = null
     this.pass = null
 
+    this.params = {
+      token: null
+    }
+
     this.accountId = null
-    this.token = null
     this.appkey = null
     this.appkeySalt = null
+
+    this.socket = new Socket(`${process.env.VUE_APP_WS_URL}/socket`, {
+      params: () => this.params
+    })
+
+    this.socket.onError((e) => {
+      console.log('Socket error!', e)
+    })
   }
 
   async connect (email, pass) {
@@ -19,43 +32,98 @@ export default class {
 
     const authResp = await userClient.authenticate(email, pass)
 
-    this.token = authResp.token
+    this.params.token = authResp.token
     this.accountId = authResp.account_id
 
     this.userKey = await crypto.deriveKey(pass, authResp.appkey_salt)
     this.appKeysPackage = JSON.parse(await crypto.decrypt(this.userKey, authResp.appkey))
 
-    this.socket = new Socket(`${process.env.VUE_APP_WS_URL}/socket`, {
-      params: { token: this.token }
-      // logger: (kind, msg, data) => { console.log(`${kind}: ${msg}`, data) }
-    })
-
     this.socket.connect()
 
     this.channel = this.socket.channel(`item:${this.accountId}`)
 
-    this.channel.on('all_items', (items) => {
-      console.log('Receiving all items!', items)
+    this.channel.on('all_items', async (data) => {
+      const items = await Promise.all(data.items.map(async (item) => {
+        item.content = JSON.parse(
+          await crypto.decrypt(this.appKeysPackage.appKey, item.content)
+        )
+
+        return item
+      }))
+
+      this._trigger('all_items', items)
+    })
+
+    this.channel.on('add_item', async ({ item }) => {
+      item.content = JSON.parse(
+        await crypto.decrypt(this.appKeysPackage.appKey, item.content)
+      )
+
+      this._trigger('add_item', item)
+    })
+
+    this.channel.on('update_item', async ({ item }) => {
+      item.content = JSON.parse(
+        await crypto.decrypt(this.appKeysPackage.appKey, item.content)
+      )
+
+      this._trigger('update_item', item)
+    })
+
+    this.channel.on('delete_item', (data) => {
+      this._trigger('delete_item', data.id)
     })
 
     this.channel.join()
-      .receive('ok', (items) => {
-        console.log('OK all items!', items)
+      .receive('error', (e) => {
+        console.log('Joining went wrong!', e)
       })
 
     return this.channel
+  }
+
+  on (event, callback) {
+    this.bindings.push({ event, callback })
+  }
+
+  _trigger (event, payload) {
+    const eventBindings = this.bindings.filter(bind => bind.event === event)
+
+    for (let i = 0; i < eventBindings.length; i++) {
+      const bind = eventBindings[i]
+
+      bind.callback(payload)
+    }
+  }
+
+  setupChannel () {
+    this.channel = this.socket.channel(`item:${this.accountId}`)
+    this.channel.on('all_items', (items) => {
+      console.log('Receiving all items!', items)
+    })
+    this.channel.join()
+      .receive('ok', (p) => {
+        console.log('Joining went well!', p)
+      })
   }
 
   async create (item, type = 'item') {
     const encryptedItem = await crypto.encrypt(this.appKeysPackage.appKey, JSON.stringify(item))
     const typeHash = await crypto.getHash(type, this.appKeysPackage.appSalt)
 
-    return this.channel.push('add_item', {
+    const resp = await this.channel.push('add_item', {
       item: {
         content: encryptedItem,
         content_type: typeHash
       }
     })
+
+    switch (resp.event) {
+      case 'add_item':
+        return resp.payload()
+      case 'error':
+        throw new Error(resp.payload())
+    }
   }
 
   async update (id, item, type) {
